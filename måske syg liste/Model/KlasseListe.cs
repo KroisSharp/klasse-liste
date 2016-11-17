403e2c06258d2a05d2fdb00ab459b3b04e372a7c
Mik Kröis Pedersen <mikx0067@edu.easj.dk>
2016-11-17 10:17:37 +0100

    lavet ctor og tilføjet værdier

--- måske syg liste/Model/KlasseListe.cs
+++ måske syg liste/Model/KlasseListe.cs
@@ -7,8 +7,19 @@ using System.Collections.ObjectModel;
 
 namespace måske_syg_liste.Model
 {
-   public class KlasseListe : ObservableCollection<klasseinfo>
+    public class KlasseListe : ObservableCollection<klasseinfo>
     {
-
+        public KlasseListe() : base()
+        {
+            this.Add(new klasseinfo()
+            {
+                FirstName = "fornavn",
+                LastName = "efternavn",
+                Mobil = 12345678,
+                Email = "mail@2.me",
+                GitHubNavn = "super Gitte"
+            }
+            );
+        }
     }
 }
