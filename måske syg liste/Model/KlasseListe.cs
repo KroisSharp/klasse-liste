a5205fef758252f772dfdc4c1e849ce6829aa4ab
Mik Kröis Pedersen <mikx0067@edu.easj.dk>
2016-11-17 10:00:32 +0100

    start

---
+++ måske syg liste/Model/KlasseListe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace måske_syg_liste.Model
+{
+   public class KlasseListe : ObservableCollection<klasseinfo>
+    {
+
+    }
+}
---
+++ måske syg liste/Model/klasseinfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace måske_syg_liste.Model
+{
+    public class klasseinfo
+    {
+
+        //props til brug af liste
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Mobil { get; set; }
+        public string Email { get; set; }
+        public string GitHubNavn { get; set; }
+    }
+}
