ac55f11b6164b4989cc66b67e47f838c37ef39a0
Mik Kröis Pedersen <mikx0067@edu.easj.dk>
2016-11-17 11:30:55 +0100

    smidt inotifyproppertychanged ind

--- måske syg liste/ViewModel/KlasseViewmodel.cs
+++ måske syg liste/ViewModel/KlasseViewmodel.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace måske_syg_liste.ViewModel
 {
-    class KlasseViewmodel
+    class KlasseViewmodel : INotifyPropertyChanged
     {
         public Model.KlasseListe PListe { get; set; }
 
@@ -17,13 +18,27 @@ namespace måske_syg_liste.ViewModel
         public Model.klasseinfo selectedElev
         {
             get { return SelectedElev; }
-            set { SelectedElev = value; }
+            set {
+                SelectedElev = value;
+                OnPropertyChanged(nameof(selectedElev));
+            }
         }
 
 
         public KlasseViewmodel()
         {
             PListe = new Model.KlasseListe();
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
         }
 
     }
