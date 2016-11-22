2eab908258ab232b955a5f26535982d6539aba2e
Mik Kröis Pedersen <mikx0067@edu.easj.dk>
2016-11-22 10:10:45 +0100

    oprettet ny class i modelview

---
+++ måske syg liste/ViewModel/AddElevCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace måske_syg_liste.ViewModel
+{
+    public class AddElevCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        //bruges til at fortælle må jeg overhoved trykke på denne her knap
+        // kan man add ting fx er der skrevet noget i det felter der skal stå noget i
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
